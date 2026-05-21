60202a44f3ae1278b67ae0662e30212133b5d63e
akshaynarote1411 <akshaynarote1411@gmail.com>
2026-05-21 11:24:28 +0530

    Add files via upload

---
+++ ex2.py
@@ -0,0 +1,15 @@
+class MATH:
+    def add(self,a,b):
+        print('the addition of:',a+b)
+    def sub(self,a,b):
+        print('the Subtraction of:',a-b)
+    def mul(self,a,b):
+        print('the multilication of:',a*b)
+    def div(self,a,b):
+        print('the division of:',a/b)
+
+obj=MATH()
+obj.add(10,20)
+obj.sub(20,30)
+obj.mul(40,50)
+obj.div(100,20)
