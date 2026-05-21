ba65dc60bb4bfcbc487fadcfce0ef8cd7b503c3f
akshaynarote1411 <akshaynarote1411@gmail.com>
2026-05-21 11:24:46 +0530

    Add files via upload

---
+++ ex3.py
@@ -0,0 +1,23 @@
+# class level arguments
+# we will define class arguments by using __init__
+# these areguments directly use by any function
+# but while using these aregumts we need display as **self.arg**
+
+class MATH:
+    def __init__(self,a,b):
+        self.a=a
+        self.b=b
+    def add(self,c):
+        print('the addition of:',self.a + self.b + c)
+    def sub(self):
+        print('the Subtraction of:',self.a-self.b)
+    def mul(self):
+        print('the multilication of:',self.a*self.b)
+    def div(self):
+        print('the division of:',self.a/self.b)
+
+obj=MATH(100,200)
+obj.add(300)
+obj.sub()
+obj.mul()
+obj.div()
